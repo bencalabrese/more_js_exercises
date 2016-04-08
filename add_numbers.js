8be99e66890e7c343e84ae57baed3e60b30946be
App Academy Student <student@appacademy.io>
2016-04-08 14:19:21 -0700

    implement number adding function

---
+++ add_numbers.js
@@ -0,0 +1,24 @@
+var readline = require('readline');
+var reader = readline.createInterface({
+  input  : process.stdin,
+  output : process.stdout
+});
+
+function addNumbers (sum, numsLeft, completionCallback) {
+    if (numsLeft < 1) {
+        completionCallback(sum);
+        reader.close();
+    } else {
+        reader.question("Num?\n", function (answer) {
+            sum += parseInt(answer);
+
+            console.log("Interim sum: " + sum);
+
+            addNumbers(sum, numsLeft - 1, completionCallback);
+        });
+    }
+}
+
+addNumbers(0, 3, function (sum) {
+  console.log("Total Sum: " + sum);
+});
