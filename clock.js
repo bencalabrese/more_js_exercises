56bc394434f6a58530bfaa0d3a8adbeca89b1e02
App Academy Student <student@appacademy.io>
2016-04-08 13:52:27 -0700

    implemented clock

--- clock.js
+++ clock.js
@@ -1,13 +1,23 @@
 function Clock () {
     var time = new Date();
-};
+    this.hours = time.getHours();
+    this.minutes = time.getMinutes();
+    this.seconds = time.getSeconds();
+    setInterval(this._tick.bind(this),1000);
+}
 
 Clock.prototype.printTime = function () {
-
+  console.log(this.hours +":"+ this.minutes +":"+ this.seconds);
 };
 
 Clock.prototype._tick = function () {
+    this.seconds += 1;
+    this.minutes += Math.floor(this.seconds/60);
+    this.hours += Math.floor(this.minutes/60);
+    this.minutes = this.minutes%60;
+    this.seconds = this.seconds%60;
 
+    this.printTime();
 };
 
 var clock = new Clock();
